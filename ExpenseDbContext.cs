ae93dc61ce18bf555366f8c414ba700dbb270e53
Josef Zizka <xzizka1@fi.muni.cz>
2022-05-10 15:38:35 +0200

    DbContext, nmot yet async

--- ExpenseDbContext.cs
+++ ExpenseDbContext.cs
@@ -11,15 +11,44 @@ namespace Project
     public class ExpenseDbContext : DbContext
     {
         private string connectionString = @"server=(localdb)\MSSQLLocalDB;
-                    Initial Catalog=PetsDB; Integrated Security=true";
+                    Initial Catalog=ExpenseManagerDB; Integrated Security=true";
 
 
         public DbSet<Expense> Expenses { get; set; }
         public DbSet<Group> Groups{ get; set; }
         public DbSet<User> Users { get; set; }
+        public ExpenseDbContext() : base()
+        {
+            Database.EnsureCreated();
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connectionString);
+
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                        .HasMany(u => u.Expenses)
+                        .WithMany(e => e.PaidFor);
+            
+            modelBuilder.Entity<User>()
+                        .HasMany(u => u.ExpensesPaid)
+                        .WithOne(e => e.Payer)
+                        .OnDelete(DeleteBehavior.Restrict);
+            
+            modelBuilder.Entity<User>()
+                        .HasMany(u => u.ManagedGroups)
+                        .WithOne(g => g.Admin)
+                        .OnDelete(DeleteBehavior.Restrict);
+                
+
+            modelBuilder.Entity<User>()
+                        .HasMany(u => u.Groups)
+                        .WithMany(g => g.Members);
+            
+           
+        }
+
     }
 }
